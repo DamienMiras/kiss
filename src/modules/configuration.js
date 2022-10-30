ce54b66b6c360b12cb70cf43b366976b03d91ffb
Damien MIRAS <damien.miras@gmail.com>
2022-10-30 23:34:01 +0100

    add configurable view and service folder

--- src/modules/configuration.js
+++ src/modules/configuration.js
@@ -1,33 +1,82 @@
-export default class Configuration {
+import logger from "./log.js";
 
+export default class Configuration {
 
 
     static get() {
 
-
         if (!window.configuration) {
-            window.configuration=  {
-                basePath : ""
-            }
+            window.configuration = new ConfigurationSingleton();
         }
         return window.configuration;
     }
 
-
-
-
-
     static getBasePath() {
-        return Configuration.get().basePath;
+        return Configuration.get().getBasePath();
     }
-
 
     static setBasePath(basePath) {
-        Configuration.get().basePath = basePath;
-        return  Configuration.get();
+        Configuration.get().setBasePath(basePath);
+        return Configuration.get();
     }
 
+    static getViewPath() {
+        return Configuration.get().getViewpath();
+    }
 
+    static setServicePath(viewPath) {
+        Configuration.get().setServicePath(viewPath);
+        return Configuration.get();
+    }
 
+    static getServicePath() {
+        return Configuration.get().getServicePath();
+    }
+
+    static setServicePath(servicePath) {
+        Configuration.get().setServicePath(servicePath);
+        return Configuration.get();
+    }
 
 }
+
+class ConfigurationSingleton {
+    #basePath = "";
+    #viewPath = "views/";
+    #servicePath = "services/";
+
+    constructor() {
+        logger.setCaller(this);
+    }
+
+    setBasePath(basePath) {
+
+        this.#basePath = basePath;
+        return this;
+    }
+
+    getBasePath() {
+
+        return this.#basePath;
+    }
+
+    setViewPath(viewPath) {
+        this.#viewPath = viewPath
+        return this;
+    }
+
+    getViewpath() {
+        return this.#viewPath
+    }
+
+    setServicePath(servicePath) {
+        this.#servicePath = servicePath
+        return this;
+    }
+
+    getServicePath() {
+        return this.#servicePath
+    }
+
+}
+
