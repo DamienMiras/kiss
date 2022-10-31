f9855c88af8918fe3aec043872b931739c44a807
Damien MIRAS <damien.miras@gmail.com>
2022-10-31 18:51:41 +0100

    provide a objectUtl for repetitive test

--- src/modules/objectUtil.js
+++ src/modules/objectUtil.js
@@ -1,6 +1,24 @@
 Object.prototype.hasMethod = function (methodName) {
     return ((typeof this[methodName]) == "function");
 };
-let dummy = function () {
+let isNotEmpty = function (value) {
+    if (value === null || value === undefined) {
+        return false;
+    }
+    if ((typeof value === 'string' || value instanceof String) && value.length === 0) {
+        return false;
+    }
+
+    if (Array.isArray(value) && value.length === 0) {
+        return false;
+    }
+    return true;
 }
-export {dummy as ObjectUtil};
+let isObject = function (value) {
+    return typeof value === 'object' &&
+        !Array.isArray(value) &&
+        value !== null
+}
+let ObjectUtil = function () {
+}
+export {isNotEmpty, ObjectUtil, isObject};
